28872ec3b8e0792fb742b53658cadcb71fda2f9e
Simon Tian <tianxu03@gmail.com>
2021-08-16 18:58:20 +0800

    update

--- GeometricRNG/test/sample-test.js
+++ GeometricRNG/test/sample-test.js
@@ -1,14 +1,8 @@
 const { expect } = require("chai");
+const { ethers } = require("hardhat");
+const { utils } = require('ethers');
+const [owner, addr1] = await ethers.getSigners();
 
-describe("Greeter", function() {
-  it("Should return the new greeting once it's changed", async function() {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    
-    await greeter.deployed();
-    expect(await greeter.greet()).to.equal("Hello, world!");
-
-    await greeter.setGreeting("Hola, mundo!");
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
-  });
-});
+const GeometricRNG = await hre.ethers.getContractFactory("GeometricRNG");
+const GeometricRNGD = await GeometricRNG.deploy();
+const GeometricRNGDAddr = GeometricRNGD.address;
